8639272edde46e12b985f2a3447ab7db56621d40
dhairyapatel1506 <ts554408@gmail.com>
2022-08-20 23:17:56 +0530

    Can now encrypt entire directories (including subdirectories)

--- decryptor.py
+++ decryptor.py
@@ -6,16 +6,24 @@ from cryptography.fernet import Fernet
 files = []
 keysf = 'keys.key'
 
+def addFiles(file):
+        if os.path.isfile(file):
+            files.append(file)
+        elif os.path.isdir(file):
+            dir = file
+            for aFile in os.listdir(dir):
+                theFile = f"{dir}/{aFile}"
+                addFiles(theFile)
+
 # Collecting the files in the current directory
 if not os.path.exists(keysf):
     print("Files are not encrypted!")
     
 else:
     for file in os.listdir():
-        if file == "encryptor.py" or file == "keys.key" or file == "decryptor.py" or file == "LICENSE" or file == "README.md":
+        if file == "encryptor.py" or file == "keys.key" or file == "decryptor.py" or file == "LICENSE" or file == "README.md" or file == ".git":
             continue
-        if os.path.isfile(file):
-            files.append(file)
+        addFiles(file)
 
     with open(keysf, "rb") as keys:
         pKey, pwd, key = keys.readlines()
@@ -34,7 +42,7 @@ else:
             with open(file, "wb") as f:
                 f.write(decrypted_contents)
 
-        print("The following files have been decrypted: ")
+        print("\nThe following files have been decrypted: ")
         for file in files:
             print(f"\t-{file}")
 
--- encryptor.py
+++ encryptor.py
@@ -6,72 +6,81 @@ from cryptography.fernet import Fernet
 files = []
 keysf = "keys.key"
 
+def addFiles(file):
+        if os.path.isfile(file):
+            files.append(file)
+        elif os.path.isdir(file):
+            dir = file
+            for aFile in os.listdir(dir):
+                theFile = f"{dir}/{aFile}"
+                addFiles(theFile)
+
 # Collecting the files in the current directory
 if os.path.exists(keysf):
     print("Files are already encrypted!")
 
 else:
-    open(keysf, "w").close
-
     for file in os.listdir():
-        if file == "encryptor.py" or file == "decryptor.py" or file == "keys.key" or file == "LICENSE" or file == "README.md":
+        if file == "encryptor.py" or file == "decryptor.py" or file == "keys.key" or file == "LICENSE" or file == "README.md" or file == ".git":
             continue
-        if os.path.isfile(file):
-            files.append(file)
+        addFiles(file)
 
     pwd = input("Enter a password: ")
 
-    # Generating Password Encryption Key
-    pKey = Fernet.generate_key()
+    if(pwd):
+        open(keysf, "w").close
 
-    with open(keysf, "wb") as keys:
-        keys.write(pKey)
+        # Generating Password Encryption Key
+        pKey = Fernet.generate_key()
 
-    with open(keysf, "a") as keys:
-        keys.write("\n")
+        with open(keysf, "wb") as keys:
+            keys.write(pKey)
 
-    # Encrypting the Password
-    pwd = Fernet(pKey).encrypt(pwd.encode())
+        with open(keysf, "a") as keys:
+            keys.write("\n")
 
-    with open(keysf, "ab") as keys:
-        keys.write(pwd)
+        # Encrypting the Password
+        pwd = Fernet(pKey).encrypt(pwd.encode())
 
-    with open(keysf, "a") as keys:
-        keys.write("\n")
+        with open(keysf, "ab") as keys:
+            keys.write(pwd)
 
-    # Generating File Encryption Key
-    key = Fernet.generate_key()
+        with open(keysf, "a") as keys:
+            keys.write("\n")
 
-    with open(keysf, "ab") as keys:
-        keys.write(key)
+        # Generating File Encryption Key
+        key = Fernet.generate_key()
 
-    with open(keysf, "a") as keys:
-        keys.write("\n")
+        with open(keysf, "ab") as keys:
+            keys.write(key)
 
-    # Encrypting the files
-    for file in files:
-        with open(file, "rb") as f:
-            contents = f.read()
-        encrypted_contents = Fernet(key).encrypt(contents)
-        with open(file, "wb") as f:
-            f.write(encrypted_contents)
-        with open(file, "a") as f:
-            f.write("\n")
+        with open(keysf, "a") as keys:
+            keys.write("\n")
 
-    print("The following files have been encrypted: ")
+        # Encrypting the files
+        for file in files:
+            with open(file, "rb") as f:
+                contents = f.read()
+            encrypted_contents = Fernet(key).encrypt(contents)
+            with open(file, "wb") as f:
+                f.write(encrypted_contents)
+            with open(file, "a") as f:
+                f.write("\n")
 
-    for file in files:
-        print(f"\t-{file}")
+        print("\nThe following files have been encrypted: ")
 
-    while(True):
-        sf = input("Self-destruct? (y/n) ")
-        if sf == 'y':
-            os.remove("encryptor.py")
-            os.remove("decryptor.py")
-            os.remove("keys.key")
-            exit()
-        elif sf == 'n':
-            exit()
-        else:
-            print("Incorrect input. Try again.\n")
-            continue   
+        for file in files:
+            print(f"\t-{file}")
+
+        while(True):
+            sf = input("\nSelf-destruct? (y/n) ")
+            if sf == 'y':
+                os.remove("encryptor.py")
+                os.remove("decryptor.py")
+                os.remove("keys.key")
+                exit()
+            elif sf == 'n':
+                exit()
+            else:
+                print("Incorrect input. Try again.\n")
+                continue
